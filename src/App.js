0322b0094a4baee57f3f7cf7f8e42eb50cd6eadd
MOAAS <miguel.moas@gmail.com>
2021-12-09 18:37:33 +0000

    Mega fix to projectmoji

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import axios from "axios"
-
 function App() {
-  const onClick = () => {
-    axios.get("/hello").then(res => console.log(res.data))
-  }
   return (
     <div className="App">
       <header className="App-header">
@@ -14,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div onClick={onClick}>Click me</div>
         <a
           className="App-link"
           href="https://reactjs.org"
